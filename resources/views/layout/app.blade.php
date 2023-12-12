cd55cc39f69532db31461fb81a0c8a6d1e863f39
Rodel Santos <rodelsantos1999@gmail.com>
2023-12-12 17:03:03 +0800

    save

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <!-- jQuery library -->
+
+    <!-- Select2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Select2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
+
+    <!-- DataTables JS -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+
     <style>
         body {
             background-color: #F8F8F8;
@@ -15,7 +40,6 @@
 
 <body>
     <header>
-
     </header>
 
     <main>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,16 @@
  @extends('layout.app')
 
  @section('title', 'Performance Management System')
+ <select class="js-example-basic-single">
+     <option value="option1">Option 1</option>
+     <option value="option2">Option 2</option>
+     <!-- Add more options as needed -->
+ </select>
 
  @section('content')
-
+     <script>
+         $(document).ready(function() {
+             $('.js-example-basic-single').select2();
+         });
+     </script>
  @endsection
