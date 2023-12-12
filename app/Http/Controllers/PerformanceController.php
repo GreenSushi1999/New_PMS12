01f5da8b2b2645e68bf1c6518295d308a6d43b67
GreenSushi1999 <rodelsantos1999@gmail.com>
2023-12-12 16:31:43 +0800

    space

--- app/Http/Controllers/PerformanceController.php
+++ app/Http/Controllers/PerformanceController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\document;
 use Illuminate\Http\Request;
 
 class PerformanceController extends Controller
 {
     public function index()
     {
+
         return view('pages.index');
     }
 }
